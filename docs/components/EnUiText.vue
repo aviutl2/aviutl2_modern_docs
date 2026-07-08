d2f82d9431b3735c4c550f901b5e103ac544b731
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-07-08 19:11:26 +0900

    feat: effects.ymlもいれる

--- docs/components/EnUiText.vue
+++ docs/components/EnUiText.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { computed, defineProps } from "vue";
 import UiText from "./UiText.vue";
-import lang from "./ui/en_editor.yml";
+import enEditor from "./ui/en_editor.yml";
+import enEffects from "./ui/en_effects.yml";
 
-defineProps<{
-  scope: string;
+const lang = {
+  ...enEditor,
+  ...enEffects,
+};
+
+const props = defineProps<{
+  scope?: string;
   text: string;
 }>();
+const scope = computed(() => props.scope || props.text);
 </script>
 
 <template><UiText :lang :scope :text /></template>
